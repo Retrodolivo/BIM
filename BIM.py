e64a092b70ebc7805fa6ea06efe86f3b34e06e5a
Retrodolivo <naskin.iv@gmail.com>
2021-02-15 19:39:52 +0300

    BIM.py add peridical adc read

--- BIM.py
+++ BIM.py
@@ -6,14 +6,32 @@ win = tk.Tk()
 win.title("BIM GUI")
 
 ADC = ADC_ad4020.AD4020(1, 0, 0, 2000000)
+#_________global flags__________________
+adc_stop = True
+#_______________________________________
+
 
 #_______________DEFINES_________________
 def close():
     GPIO.cleanup()
     win.destroy()
 
-def adc_read():
-    adc_label["text"] = str(ADC.read())
+def adc_read_start():
+    global adc_stop
+    if adc_stop:
+        adc_label["text"] = str(ADC.read())
+        adc_btn_stop["state"] = "normal"
+        adc_btn_start["state"] = "disabled"
+        win.after(1000, adc_read_start)
+    else:
+        adc_stop = True
+        return
+        
+def adc_read_stop():
+    global adc_stop
+    adc_stop = False
+    adc_btn_start["state"] = "normal"
+    adc_btn_stop["state"] = "disabled"
 
 def dac_set():
     pass
@@ -37,9 +55,12 @@ dac_ad4020_lframe.grid(row = 0, column = 1, pady = 10, padx = 10)
 adc_label = tk.Label(dac_ad4020_lframe, text = "_.______ V", height = 2, font = "Arial 20 bold", bg = "pink")
 adc_label.grid(in_ = dac_ad4020_lframe, pady = 10, padx = 10)
 
-adc_btn = tk.Button(text = "Start", font = "Arial 11", width = 7, command = adc_read)
-adc_btn.bind("<Button-1>")
-adc_btn.grid(in_ = dac_ad4020_lframe, pady = 10, padx = 10)
+adc_btn_start = tk.Button(text = "Start", font = "Arial 11", width = 7, command = adc_read_start)
+adc_btn_start.bind("<Button-1>")
+adc_btn_start.grid(in_ = dac_ad4020_lframe)
+adc_btn_stop = tk.Button(text = "Stop", font = "Arial 11", width = 7, command = adc_read_stop)
+adc_btn_stop.bind("<Button-1>")
+adc_btn_stop.grid(in_ = dac_ad4020_lframe)
 
 #--------------DAC_AD5543-------------------#
 dac_ad5543_lframe = tk.LabelFrame(win, text = "DAC_AD5543", font = "Arial 12 bold", width=200, height=100)
@@ -63,5 +84,5 @@ exit_btn.bind("<Button-1>")
 exit_btn.grid(row = 2, column = 1)
 
 
-win.geometry("350x330")
+win.geometry("350x370")
 win.mainloop()
